d9ca74d14a281bd87c83eb5e5a04b5f47ec2c01a
hyunsooMe <khyunsooya@naver.com>
2024-05-17 22:53:33 +0900

    clear

--- todolist/views.py
+++ todolist/views.py
@@ -5,18 +5,18 @@ from .models import *
 
 # Create your views here.
 def index(request):
-    todos = Todo.objects.all()
+    todos = Todolist.objects.all()
     content = {'todos': todos}
     return render(request, 'todolist/index.html', content)
 def createTodo(request):
     user_input_str = request.POST['todoContent']
-
-    new_todo = Todo(content = user_input_str)
+    new_todo = Todolist(content = user_input_str)
     new_todo.save()
     return HttpResponseRedirect(reverse('index'))
 
 def deleteTodo(request):
     delete_todo_id= request.GET['todoNum']
-    todo = Todo.objects.get(id = delete_todo_id)
+    todo = (Todolist
+            .objects.get(id = delete_todo_id))
     todo.delete()
     return HttpResponseRedirect(reverse('index'))
